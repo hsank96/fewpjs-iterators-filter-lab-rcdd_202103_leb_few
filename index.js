a93086d1a90e8d0f834879f69830e65faed39c40
Hazem El Sankari <hazemleb96@gmail.com>
2021-05-02 14:33:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+
+function findMatching(drivers,nameToCheck){
+  let filtered = drivers.filter(n =>{return n.toLowerCase()===nameToCheck.toLowerCase();});
+  return filtered;
+}
+
+findMatching(drivers,'Bobby')
+
+function fuzzyMatch(drivers, check){
+  let filtered = drivers.filter(n =>{return n.toLowerCase()===nameToCheck.toLowerCase();});
+  return filtered;
+}
